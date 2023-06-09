9b2553dc80030f13b4a9bf70addf300e326e7a0d
aisaev <an.isaev7@gmail.com>
2023-06-09 12:35:02 +0300

    Add admin fields

--- delivery/admin.py
+++ delivery/admin.py
@@ -6,14 +6,21 @@ from delivery.models import Delivery, Material, Supplier
 # Register your models here.
 @admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'country']
 
 
 @admin.register(Material)
 class SupplierAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['sap_number', 'description', 'supplier']
 
 
 @admin.register(Delivery)
 class SupplierAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'material',
+        'supplier',
+        'shipment_date',
+        'delivery_date',
+        'shipment_week',
+        'delivery_week',
+    ]
